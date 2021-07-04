81c44448a5bff816d9d7361ac61ee42651c2124d
KillDozerX2 <himanshupant127@gmail.com>
2021-07-04 13:21:46 +0530

    docs: :memo: Added JSDoc for type hinting and intellisense for using Figma Client

--- src/figma-client.js
+++ src/figma-client.js
@@ -1,6 +1,11 @@
 const axios = require('axios')
 const figmaApiBase = 'https://api.figma.com/v1'
 
+/**
+ * @param {String} token The Figma Api token
+ * @see {@link https://www.figma.com/developers/api#access-tokens}
+ * @returns {axios.AxiosStatic} An instance of the Axios http client
+ */
 const Client = (token) => {
 
   const instance = axios.create({
